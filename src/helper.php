62e79c4f7fe920df4e883c6a26e76d672f3548b1
kain <zhangtqx@vip.qq.com>
2020-01-19 18:27:59 +0800

    更新

--- src/helper.php
+++ src/helper.php
@@ -1,37 +1,32 @@
 <?php
 declare(strict_types=1);
 
-use \Hyperf\HttpServer\Router\Router;
+use Hyperf\HttpServer\Router\Router;
 use Hyperf\Utils\Str;
 
-if (!function_exists('addCurdRoutes')) {
+if (!function_exists('AutoController')) {
     /**
-     * Load CURD route
      * @param string $controller
      * @param array $options
      * @throws ReflectionException
      */
-    function addCurdRoutes(string $controller, array $options = []): void
+    function AutoController(string $controller, array $options = [])
     {
         $reflect = new ReflectionClass($controller);
         $path = lcfirst(Str::before($reflect->getShortName(), 'Controller'));
-        if ($reflect->hasMethod('get')) {
-            Router::post('/' . $path . '/get', [$controller, 'get'], $options);
-        }
-        if ($reflect->hasMethod('originLists')) {
-            Router::post('/' . $path . '/originLists', [$controller, 'originLists'], $options);
-        }
-        if ($reflect->hasMethod('lists')) {
-            Router::post('/' . $path . '/lists', [$controller, 'lists'], $options);
-        }
-        if ($reflect->hasMethod('add')) {
-            Router::post('/' . $path . '/add', [$controller, 'add'], $options);
-        }
-        if ($reflect->hasMethod('edit')) {
-            Router::post('/' . $path . '/edit', [$controller, 'edit'], $options);
-        }
-        if ($reflect->hasMethod('delete')) {
-            Router::post('/' . $path . '/delete', [$controller, 'delete'], $options);
+        $methods = array_filter(
+            $reflect->getMethods(ReflectionMethod::IS_PUBLIC),
+            fn($v) => !in_array($v->name, config('curd.auto.ignore'))
+        );
+        foreach ($methods as $method) {
+            Router::addRoute(
+                ['POST', 'OPTIONS'],
+                '/' . $path . '/' . $method->name,
+                [$controller, $method->name],
+                [
+                    'middleware' => []
+                ]
+            );
         }
     }
 }
