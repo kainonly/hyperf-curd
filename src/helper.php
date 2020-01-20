2e1bc1546cf49c1b62f8b70f5b1c83742a310a40
kain <zhangtqx@vip.qq.com>
2020-01-20 14:50:46 +0800

    更新路由

--- src/helper.php
+++ src/helper.php
@@ -16,15 +16,25 @@ if (!function_exists('AutoController')) {
         $path = lcfirst(Str::before($reflect->getShortName(), 'Controller'));
         $methods = array_filter(
             $reflect->getMethods(ReflectionMethod::IS_PUBLIC),
-            fn($v) => !in_array($v->name, config('curd.auto.ignore'))
+            fn($v) => !in_array($v->name, config('curd.auto.ignore'), true)
         );
+        $middlewares = $options['middleware'] ?? [];
         foreach ($methods as $method) {
+            $middleware = [];
+            foreach ($middlewares as $key => $value) {
+                if (is_string($value)) {
+                    $middleware[] = $value;
+                }
+                if (is_array($value) && in_array($method->name, $value, true)) {
+                    $middleware[] = $key;
+                }
+            }
             Router::addRoute(
                 ['POST', 'OPTIONS'],
                 '/' . $path . '/' . $method->name,
                 [$controller, $method->name],
                 [
-                    'middleware' => []
+                    'middleware' => $middleware
                 ]
             );
         }
